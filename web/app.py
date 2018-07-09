d1019a48bba25520bb9dbd74f9516b2b22623955
Craig Martin <craig@craigmartin.com>
2018-07-09 14:38:28 -0700

    silly typos

--- web/app.py
+++ web/app.py
@@ -9,15 +9,18 @@ app = Flask(__name__)
 def hello_world():
     return 'She carried a Raspberry Flask'
 
-@app.route('/stream-start'):
+@app.route('/stream-start')
+def stream_start():
     print stream.restart()
     return 'Video stream started'
 
-@app.route('/stream-stop'):
+@app.route('/stream-stop')
+def stream_stop():
     print stream.stop()
     return 'Video stream stopped'
 
-@app.route('/connection-test'):
+@app.route('/connection-test')
+def connection_test():
     return 'OK'
 
 if __name__ == '__main__':
