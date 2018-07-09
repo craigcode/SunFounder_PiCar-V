f49e21e4d8f7416be094743982cb26f6cf8a2c00
Craig Martin <craig@craigmartin.com>
2018-07-09 13:46:20 -0700

    mjpg stream restart

--- web/app.py
+++ web/app.py
@@ -3,10 +3,11 @@ from driver import stream
 
 app = Flask(__name__)
 
+print stream.restart()
+
 @app.route('/')
 def hello_world():
     return 'She carried a Raspberry Flask'
 
 if __name__ == '__main__':
-    print stream.start()
     app.run(debug=True,host='0.0.0.0')
