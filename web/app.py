bf850643cfe79c6339f4b3c713f0f91b06508863
Craig Martin <craig@craigmartin.com>
2018-07-09 13:20:31 -0700

    first stream

--- web/app.py
+++ web/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from driver import stream
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +8,5 @@ def hello_world():
     return 'She carried a Raspberry Flask'
 
 if __name__ == '__main__':
+    print stream.start()
     app.run(debug=True,host='0.0.0.0')
