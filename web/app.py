9037fe1b820cd85b257d46d69bc9c59feebb9836
Craig Martin <craig@craigmartin.com>
2018-07-09 14:35:05 -0700

    stream control

--- web/app.py
+++ web/app.py
@@ -3,11 +3,22 @@ from driver import stream
 
 app = Flask(__name__)
 
-print stream.restart()
+#print stream.restart()
 
 @app.route('/')
 def hello_world():
     return 'She carried a Raspberry Flask'
+
+@app.route('/stream-start'):
+    print stream.restart()
+    return 'Video stream started'
+
+@app.route('/stream-stop'):
+    print stream.stop()
+    return 'Video stream stopped'
+
+@app.route('/connection-test'):
+    return 'OK'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
